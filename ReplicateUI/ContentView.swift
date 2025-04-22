e50286ad173c323d208385e3f02fe3a1ce127903
dljardim <dljardim@outlook.com>
2025-04-22 09:08:54 -0500

    Initial Commit

---
+++ ReplicateUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ReplicateUI
+//
+//  Created by Damian Jardim on 4/22/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ReplicateUI/ReplicateUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReplicateUIApp.swift
+//  ReplicateUI
+//
+//  Created by Damian Jardim on 4/22/25.
+//
+
+import SwiftUI
+
+@main
+struct ReplicateUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
