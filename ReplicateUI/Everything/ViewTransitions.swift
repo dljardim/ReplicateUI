c4466a492a599864a87a860fdb432b7c182e1abd
dljardim <dljardim@outlook.com>
2025-05-15 10:05:31 -0500

    added ViewTransitions

---
+++ ReplicateUI/Everything/ViewTransitions.swift
@@ -0,0 +1,18 @@
+//
+//  ViewTransitions.swift
+//  ReplicateUI
+//
+//  Created by Damian Jardim on 5/15/25.
+//
+
+import SwiftUI
+
+struct ViewTransitions: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ViewTransitions()
+}
---
+++ YesChef/YesChef.swift
@@ -0,0 +1,18 @@
+//
+//  YesChef.swift
+//  ReplicateUI
+//
+//  Created by Damian Jardim on 5/13/25.
+//
+
+import SwiftUI
+
+struct YesChef: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    YesChef()
+}
